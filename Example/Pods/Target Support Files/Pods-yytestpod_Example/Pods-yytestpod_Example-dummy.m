163378f774c98eaeec592b5964e94a8921524da5
derek <>
2022-11-01 14:18:49 +0800

    init

---
+++ Example/Pods/Target Support Files/Pods-yytestpod_Example/Pods-yytestpod_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_yytestpod_Example : NSObject
+@end
+@implementation PodsDummy_Pods_yytestpod_Example
+@end
---
+++ Example/Pods/Target Support Files/yytestpod/yytestpod-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_yytestpod : NSObject
+@end
+@implementation PodsDummy_yytestpod
+@end
